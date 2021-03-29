864e22666ddb3041152977a047cbc53d3879980a
justNOZA <1995pjs2635@gmail.com>
2021-03-30 07:55:24 +0900

    FDA_api address and porm.xml 수정

---
+++ WebProject_No1/src/main/java/com/jaesung/park/API/FDA_Apis.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author jaron
+ *
+ */
+package com.jaesung.park.API;
+
+public class FDA_Apis {
+	private String apiAddress = "https://api.fda.gov/drug/event.json?api_key=mOq21px51a4wmrcoGOXckzjSGwoIrR80ThOrV1lx&";
+}
+/*
+ * <modelVersion>4.0.0</modelVersion>
+	<groupId>com.jaesung</groupId>
+	<artifactId>park</artifactId>
+	<name>WebProject_No1</name>
+	<packaging>war</packaging>
+	<version>1.0.0-BUILD-SNAPSHOT</version>
+ */
+*/
